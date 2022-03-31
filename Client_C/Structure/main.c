89332f6d48707f85ab4c27d81e17d18342a04255
jacejang1027 <jacejang1027@gmail.com>
2022-03-31 19:49:56 +0900

    C
    1. struct
    2. union

--- Client_C/Structure/main.c
+++ Client_C/Structure/main.c
@@ -13,11 +13,29 @@ struct Coord {
 	int y;
 };
 
-struct Student {
+typedef struct Student {
 	char name[20]; // 20 byte
-	int korean, english, math; // 12 byte
 	double average; // 8 byte
+	int korean, english, math;
+} Student;
+
+// 비트필드
+// 메모리 낭비를 줄일 수 있는 방법으로, 첫번째 멤버부터 순차적으로 비트를 할당 할 수 있다.
+// 단, 값이 해당 비트수를 넘어가게되면 오버플로가 발생하니 유의해서 사용해야한다.
+struct Time {
+	unsigned int sec : 6; // LSB ~ Bit5
+	unsigned int min : 6; // Bit6 ~ Bit11
+	unsigned int hour : 5; //Bit12 ~ BIt16 
 };
+
+
+struct StudentList {
+	struct Student students[3];
+	int num;
+} studentList;
+
+void
+SwapStudents(Student* student1, Student* student2);
 
 int main() {
 
@@ -41,7 +59,74 @@ int main() {
 	printf("Student : %d\n", sizeof(struct Student));
 
 	struct Student studentA;
-	struct Student* studentP;
+	struct Student* student_P;
+	
+	strcpy(studentA.name, "Luke");
+	studentA.english = 80;
+	studentA.korean = 60;
+	studentA.math = 40;
+	studentA.average = (double) (studentA.english + studentA.korean + studentA.math) / 3.0;
+
+	printf("%s", studentA.name);
+	printf("%lf", studentA.average);
+
+	student_P = &studentA;
+
+	// -> 연산자 ( 간접멤버연산자 : 구조체 포인터 변수의 멤버에 접근하는 연산자 )
+	
+	printf("%s", student_P->name);
+	student_P->english = 10;
+	printf("%d", student_P->english);
+	printf("%d", studentA.english);
+
+
+	// 구조체 변수의 멤버의 주소에는 접근할 수 있다.
+	int* koreanGrade = &studentA.korean;
+
+
+	printf("\n");
+	// 비트필드
+	struct Timet1;
+	struct Time t1;
+	printf("%d\n", sizeof(t1));
+
+	t1.hour = 5;
+	t1.min = 12;
+	t1.sec = 80;
+	printf("%d:%d:%d\n", t1.hour,t1.min, t1.sec);
+	//unsigned int* tmpHour = &t1.hour; 비트필드의 주소에는 접근할 수 없다.
+
+	struct Student studentB = {
+		{"Tom"},
+		{85},
+		{25},
+		{62},
+		{(double)(85 + 25 + 62) / 3.0}
+	};
+	struct Student studentC = {
+		{"Tom"},
+		{85},
+		{25},
+		{62},
+		{(double)(85 + 25 + 62) / 3.0}
+	};
+
+	// 구조체를 멤버로가지는 구조체
+	studentList.num = 3;
+	studentList.students[0] = studentA;
+	studentList.students[1] = studentB;
+	studentList.students[2] = studentC;
+	printf("stdent A' english grade %d\n", studentList.students[0].english);
+
+	// 구조체를 스왑하는 함수
+	
+	struct StudentList {
+		struct Student students[3];
+		int num;
+	} studentList;
+
+
+
 
 
 	int a = 1, b = 5;
@@ -52,4 +137,11 @@ int main() {
 	*ap = 3;
 	*bp = 8;
 
+}
+
+void SwapStudents(Student* student1, Student* student2)
+{
+	Student tmpStudent = *student1;
+	*student1 = *student2;
+	*student2 = tmpStudent;
 }
