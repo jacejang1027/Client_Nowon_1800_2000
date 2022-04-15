ead83190505942a590d87e3f2f3d93e12cf0cdb9
jacejang1027 <jacejang1027@gmail.com>
2022-04-15 19:56:08 +0900

    CPP
    1.class

--- Client_CPP/Class/main.cpp
+++ Client_CPP/Class/main.cpp
@@ -1,21 +1,25 @@
-#include <iostream>
+#pragma warning (disable : 4996)
 
+#include <iostream>
+#include "Warrior.h"
+#include "Debuffer.h"
+#include <cstring>
 using namespace std;
 
 // 접근제한자
-// pubilc : 외부에서 접근 가능한 키워드
+// public : 외부에서 접근 가능한 키워드
 // private : 외부에서 접근 불가능한 키워드
-// protected : 자식객체만 접근 가능한 키워드
+// protected : 자식객체만 접근가능한 키워드
 
 class Coord {
 
-private :
+private:
 	int _x, _y;
 
-public :
-	// 객체지향컨셉에서는
+public:
+	// 객체지향컨셉에서는 
 	// 되도록 객체가 다른 객체의 멤버변수를 직접 접근해서 수정하지 않도록 지향함
-	// 보통 접근할 수 있는 함수를 제공하도록함.
+	// 보통 접근할수있는 함수를 제공하도록함.
 	void SetCoord(int x, int y) {
 		_x = x;
 		_y = y;
@@ -24,13 +28,12 @@ public :
 	int GetCoordX() {
 		return _x;
 	}
-
 	int GetCoordY() {
 		return _y;
 	}
 
 	// 생성자 (얘는 함수)
-	// 객체 생성시 (메모리공간 확보시) 자동으로 호출
+	// 객체 생성시 (메모리공간 확보시) 자동으로 호출 
 	Coord() {
 		SetCoord(0, 0);
 	};
@@ -40,7 +43,7 @@ public :
 		SetCoord(x, y);
 	}
 
-	// 소멸자 (얘도 함수)
+	//소멸자 (얘도 함수)
 	// 객체 해제시 (메모리공간 해제시) 자동으로 호출
 	~Coord() {};
 
@@ -51,49 +54,59 @@ public :
 		// this 포인터
 		// 현재 객체 자기자신을 가리키는 포인터
 		// 클래스안에서 멤버에 접근할때는 this 포인터가 생략되어있다. 
+		return*this;
 	}
 
-	void Swap(const Coord& _other) {
-		int tmpX =this->_x; 
-		int tmpY = this->_y;
-		_x = _other._x;
-		_y = _other._y;
-	}
-	
 	// 연산자 정의
 	const Coord& operator + (const Coord& _other) {
 		Coord tmpCoord;
 		tmpCoord.SetCoord(_x + _other._x,
-						  _y + _other._y);
+			_y + _other._y);
 		return tmpCoord;
 	}
-
 };
-
 
 int main() {
 
 	Coord coord1;
 
-	cout << "(" << coord1.GetCoordX() << "." << coord1.GetCoordY() << ")" << endl;
+	cout << "(" << coord1.GetCoordX() << "," << coord1.GetCoordY() << ")" << endl;
 	coord1.SetCoord(1, 2);
-	cout << "(" << coord1.GetCoordX() << "." << coord1.GetCoordY() << ")" << endl;
+	cout << "(" << coord1.GetCoordX() << "," << coord1.GetCoordY() << ")" << endl;
 
 	// 객체를 힙영역에 동적할당 할 시
-	// new 생성자
+	// new 생성자 
 	// 형태로 할당함.
 	Coord* coord2ptr = new Coord(5, 2);
-	cout << "(" << coord2ptr->GetCoordX() << "." << coord2ptr->GetCoordY() << ")" << endl;
-	
+	cout << "(" << coord2ptr->GetCoordX() << "," << coord2ptr->GetCoordY() << ")" << endl;
+
 	// 클래스간의 대입연산 가능
 	coord1 = *coord2ptr;
-	(coord1 + *coord2ptr);
+	Coord coord3 = (coord1 + *coord2ptr);
 
+	cout << "(" << coord1.GetCoordX() << "," << coord1.GetCoordY() << ")" << endl;
 
-	cout << "(" << coord2ptr->GetCoordX() << "." << coord2ptr->GetCoordY() << ")" << endl;
-
-	
 	delete coord2ptr;
+
+
+	//Warrior* warrior = new Warrior();
+	Debuffer* debuffer = new Debuffer();
+	//debuffer->DecreaseHP(*warrior);
+	debuffer->DecreaseHP();
+	//delete warrior;
+	delete debuffer;
+
+	Debuffer debuffer1;
+	debuffer1._damage = 10;
+	strcpy(debuffer1._name, "하급디버퍼");
+
+	Debuffer debuffer2 = debuffer1;
+	std::cout << debuffer2._damage << "," << debuffer2._name << std::endl;
+
+	strcpy(debuffer2._name, "상급디버퍼");
+	debuffer2._damage = 30;
+	std::cout << "1번 디버퍼" << debuffer1._damage << "," << debuffer1._name << std::endl;
+	std::cout << "2번 디버퍼" << debuffer2._damage << "," << debuffer2._name << std::endl;
 
 	return 0;
 }
